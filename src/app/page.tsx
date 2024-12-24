73cc35dc9a7b61104d5906c7ab1ef5d523f04aa4
Diego Vilhalva <diegorocha450@yahoo.com>
2024-12-24 08:31:53 -0300

    testimonials section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Features from "@/sections/Features"
 import Header from "@/sections/Header"
 import Hero from "@/sections/Hero"
 import Pricing from "@/sections/Pricing"
+import Testimonials from "@/sections/Testimonials"
 
 export default function Home() {
   return <>
@@ -11,5 +12,6 @@ export default function Home() {
     <Companies />
     <Features />
     <Pricing />
+    <Testimonials />
   </>
 }
