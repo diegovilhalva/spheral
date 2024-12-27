e6c6fadfc4c13b9e2a34d9b635826b81081ea9b8
Diego Vilhalva <diegorocha450@yahoo.com>
2024-12-27 10:51:11 -0300

    hero animation completed

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,17 +1,70 @@
+"use client"
 import robotImg from "@/assets/images/robot.jpg"
 import undelineImage from "@/assets/images/underline.svg?url"
-import Loader from "@/assets/images/loader.svg"
+import Loader from "@/assets/images/loader-animated.svg"
 import { Button } from "@/components/Button";
 import Image from "next/image";
 import Orbit from "@/components/Orbit";
 import Planet from "@/components/Planet"
 import SectionBorder from "@/components/SectionBorder";
 import SectionContent from "@/components/SectionContent";
+import { motion, useMotionValue, useMotionValueEvent, useScroll, useSpring, useTransform } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
+const useMousePosition = () => {
+  const [innerWidth, setInnerWidth] = useState(1)
+  const [innerHeight, setInnerHeight] = useState(1)
+  const clientX = useMotionValue(0)
+  const clientY = useMotionValue(0)
+  const xProgress = useTransform(clientX, [0, innerWidth], [0, 1])
+  const yProgress = useTransform(clientY, [0, innerHeight], [0, 1])
+
+  useEffect(() => {
+    setInnerWidth(window.innerWidth)
+    setInnerHeight(window.innerHeight)
+
+    window.addEventListener('resize', () => {
+      setInnerWidth(window.innerWidth)
+      setInnerHeight(window.innerHeight)
+    })
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('mousemove', (e) => {
+      clientX.set(e.clientX)
+      clientY.set(e.clientY)
+    })
+  }, [])
+
+  return { xProgress, yProgress }
+}
 
 export const Hero = () => {
+  const { xProgress, yProgress } = useMousePosition()
+  const sectionRef = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ['end start', 'start end']
+  })
+
+  const transformedY = useTransform(scrollYProgress, [0, 1], [200, -200])
+
+  const springX = useSpring(xProgress)
+
+  const springY = useSpring(yProgress)
+
+  const translateLargeX = useTransform(springX, [0, 1], ['-25%', '25%'])
+
+  const translateLargeY = useTransform(springY, [0, 1], ['-25%', '25%'])
+
+  const translateMediumX = useTransform(springX, [0, 1], ['-50%', '50%'])
+  const translateMediumY = useTransform(springY, [0, 1], ['-50%', '50%'])
+
+  const translateSmallX = useTransform(springX, [0, 1], ['-200%', '200%'])
+  const translateSmallY = useTransform(springY, [0, 1], ['-200%', '200%'])
+
   return (
-    <section className="">
+    <section ref={sectionRef}>
       <div className="container">
         <SectionBorder>
           <SectionContent className="relative isolate  [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]">
@@ -49,32 +102,44 @@ export const Hero = () => {
             </div>
             <div className="relative isolate max-w-5xl  mx-auto">
               <div className="absolute left-1/2 top-0">
-                <Planet size="lg" color="violet" className=" -translate-x-[316px] -translate-y-[76px] rotate-135" />
-                <Planet size="lg" color="violet" className="-translate-y-[188px] translate-x-[334px] -rotate-135" />
-                <Planet size="md" color="teal" className="-translate-y-[342px] translate-x-[486px] -rotate-135" />
-                <Planet size="sm" color="fuchsia" className="-translate-y-[372px] -translate-x-[508px] rotate-135" />
+                <motion.div style={{ x: translateLargeX, y: translateLargeY }}>
+                  <Planet size="lg" color="violet" className=" -translate-x-[316px] -translate-y-[76px] rotate-135" />
+                </motion.div>
+                <motion.div style={{ x: translateLargeX, y: translateLargeY }}>
+                  <Planet size="lg" color="violet" className="-translate-y-[188px] translate-x-[334px] -rotate-135" />
+                </motion.div>
+                <motion.div style={{ x: translateMediumX, y: translateMediumY }}>
+                  <Planet size="md" color="teal" className="-translate-y-[342px] translate-x-[486px] -rotate-135" />
+                </motion.div>
+                <motion.div style={{ x: translateSmallX, y: translateSmallY }}>
+                  <Planet size="sm" color="fuchsia" className="-translate-y-[372px] -translate-x-[508px] rotate-135" />
+                </motion.div>
               </div>
               <div className="absolute left-0 z-10 top-[30%] -translate-x-10 hidden lg:block">
-                <div className="bg-gray-800/70 backdrop-blur-md border-gray-700 rounded-xl p-4 w-72">
+                <motion.div className="bg-gray-800/70 backdrop-blur-md border-gray-700 rounded-xl p-4 w-72" style={{
+                  y: transformedY
+                }}>
                   <div>Can you generate an incredible frontend dev video tutorial?</div>
                   <div className="text-right text-gray-400 text-sm font-semibold">1m ago</div>
-                </div>
+                </motion.div>
               </div>
-              <div className="absolute right-0 z-10 top-[50%] translate-x-10 hidden lg:block">
+              <motion.div className="absolute right-0 z-10 top-[50%] translate-x-10 hidden lg:block" style={{
+                y: transformedY
+              }}>
                 <div className="bg-gray-800/70 backdrop-blur-md border-gray-700 rounded-xl p-4 w-72">
                   <div>
                     <strong>Brainwave:</strong> I created one based on videos from Frontend Tribe!
                   </div>
                   <div className="text-right text-gray-400 text-sm font-semibold">Just now</div>
                 </div>
-              </div>
+              </motion.div>
               <div className="mt-20 rounded-2xl border-2 overflow-hidden border-gradient relative flex">
                 <Image src={robotImg} alt="Robot image" />
                 <div className="absolute bottom-2 md:bottom-4 lg:bottom-10 left-1/2 -translate-x-1/2 w-full max-w-xs px-4 p-[15px]">
                   <div className="bg-gray-950/80 flex items-center gap-4 px-4 py-2 rounded-2xl 
                 w-[320px] max-w-full">
                     <Loader className="text-violet-400" />
-                    <div className="font-semibold text-xl text-gray-100">AI is generating<span>|</span>
+                    <div className="font-semibold text-xl text-gray-100">AI is generating<span className="animate-cursor-blink">|</span>
                     </div>
                   </div>
                 </div>
